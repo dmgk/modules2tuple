a0cdb416ca2cbf6d3dad67a97f4fdcfac954503e
Dmitri Goutnik <dg@syrec.org>
2019-10-21 09:48:13 -0500

    sort mirrors

--- tuple/mirrors.go
+++ tuple/mirrors.go
@@ -10,9 +10,10 @@ var mirrors = map[string]struct {
 	"contrib.go.opencensus.io/exporter/ocagent": {source: GH{}, account: "census-ecosystem", project: "opencensus-go-exporter-ocagent"},
 	"docker.io/go-docker":                       {source: GH{}, account: "docker", project: "go-docker"},
 	"git.apache.org/thrift.git":                 {source: GH{}, account: "apache", project: "thrift"},
-	"go.opencensus.io":                          {source: GH{}, account: "census-instrumentation", project: "opencensus-go"},
 	"go.elastic.co/apm":                         {source: GH{}, account: "elastic", project: "apm-agent-go"},
 	"go.elastic.co/fastjson":                    {source: GH{}, account: "elastic", project: "go-fastjson"},
+	"go.mongodb.org/mongo-driver":               {source: GH{}, account: "mongodb", project: "mongo-go-driver"},
+	"go.opencensus.io":                          {source: GH{}, account: "census-instrumentation", project: "opencensus-go"},
 	"go4.org":                                   {source: GH{}, account: "go4org", project: "go4"},
 	"gocloud.dev":                               {source: GH{}, account: "google", project: "go-cloud"},
 	"google.golang.org/api":                     {source: GH{}, account: "googleapis", project: "google-api-go-client"},
@@ -20,10 +21,9 @@ var mirrors = map[string]struct {
 	"google.golang.org/genproto":                {source: GH{}, account: "google", project: "go-genproto"},
 	"google.golang.org/grpc":                    {source: GH{}, account: "grpc", project: "grpc-go"},
 	"gopkg.in/fsnotify.v1":                      {source: GH{}, account: "fsnotify", project: "fsnotify"}, // fsnotify is a special case in gopkg.in
-	"sigs.k8s.io/yaml":                          {source: GH{}, account: "kubernetes-sigs", project: "yaml"},
-	"go.mongodb.org/mongo-driver":               {source: GH{}, account: "mongodb", project: "mongo-go-driver"},
 	"gotest.tools":                              {source: GH{}, account: "gotestyourself", project: "gotest.tools"},
 	"howett.net/plist":                          {source: GL{"https://gitlab.howett.net"}, account: "go", project: "plist"},
+	"sigs.k8s.io/yaml":                          {source: GH{}, account: "kubernetes-sigs", project: "yaml"},
 }
 
 func (t *Tuple) fromMirror() bool {
